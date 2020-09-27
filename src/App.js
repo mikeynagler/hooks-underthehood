ff87bb9bcc1cc9fc08f7fda185327cf271b194f2
nagler.may@gmail.com <nagler.may@gmail.com>
2020-09-27 11:52:41 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [userQuery, setUserQuery] = useState("");
+  
+  const updateUserQuery = (event) => {
+    setUserQuery(event.target.value);
+  }
+	return <div className="App">
+    <div className="form">
+      <input type="text" value={userQuery} onChange={updateUserQuery}/>
+      <button>Search</button>
     </div>
-  );
+  </div>;
 }
 
 export default App;
